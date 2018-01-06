649eedf4d437d96cb0e97903d53fa2bf3bab84a0
Colin McArdell <colin@colinmcardell.com>
2018-01-06 18:01:01 -0500

    Reverting integration of libgroove as it does not support multichannel audio files, as well as the fact that it seems to be a largely abandoned project: 20e4cb0525b9cd1682608eff8c1c9233c22c1653, 8d31361781086d1cce1a394cea08f44f614969ff, 5571e806c8a247c2e066c4679449aedf62e84752, 5fed022d57cf60fc78d851ba06184ef251381339, a848b023975d04fd7420cb4ee3714a74fbcd60db

--- dist/cli.js
+++ dist/cli.js
@@ -15,9 +15,7 @@ var _Player = _interopRequireDefault(require("./Player"));
 /** Commands - End */
 
 /** CLI Actions */
-var lsAction =
-/*#__PURE__*/
-function () {
+var lsAction = function () {
   var _ref5 = _asyncToGenerator(
   /*#__PURE__*/
   regeneratorRuntime.mark(function _callee4(args, callback) {
@@ -44,52 +42,6 @@ function () {
 
   return function lsAction(_x2, _x3) {
     return _ref5.apply(this, arguments);
-  };
-}();
-
-var playAction =
-/*#__PURE__*/
-function () {
-  var _ref6 = _asyncToGenerator(
-  /*#__PURE__*/
-  regeneratorRuntime.mark(function _callee5(args, callback) {
-    var filePath, files, file;
-    return regeneratorRuntime.wrap(function _callee5$(_context5) {
-      while (1) {
-        switch (_context5.prev = _context5.next) {
-          case 0:
-            filePath = args && args.options && args.options.file;
-
-            if (filePath) {
-              _context5.next = 7;
-              break;
-            }
-
-            _context5.next = 4;
-            return readFiles();
-
-          case 4:
-            files = _context5.sent;
-            file = files[0];
-            /* eslint-enable prefer-destructuring */
-
-            filePath = file.path;
-
-          case 7:
-            play(filePath);
-            this.log("Playing \u2013 ".concat(player.filePath()));
-            callback();
-
-          case 10:
-          case "end":
-            return _context5.stop();
-        }
-      }
-    }, _callee5, this);
-  }));
-
-  return function playAction(_x4, _x5) {
-    return _ref6.apply(this, arguments);
   };
 }();
 
@@ -140,9 +92,7 @@ server.addCommand = function (_ref) {
 /** File System Helpers */
 
 
-var readFiles =
-/*#__PURE__*/
-function () {
+var readFiles = function () {
   var _ref2 = _asyncToGenerator(
   /*#__PURE__*/
   regeneratorRuntime.mark(function _callee2() {
@@ -157,9 +107,7 @@ function () {
             filesPath = _args2.length > 0 && _args2[0] !== undefined ? _args2[0] : FILES_PATH;
             fullFilesPath = _path.default.resolve(process.cwd(), filesPath);
             dirContent = _fs.default.readdirSync(fullFilesPath);
-            return _context2.abrupt("return", Promise.all(dirContent.map(
-            /*#__PURE__*/
-            function () {
+            return _context2.abrupt("return", Promise.all(dirContent.map(function () {
               var _ref3 = _asyncToGenerator(
               /*#__PURE__*/
               regeneratorRuntime.mark(function _callee(file) {
@@ -210,9 +158,7 @@ function () {
 /** Commands */
 
 
-var ls =
-/*#__PURE__*/
-function () {
+var ls = function () {
   var _ref4 = _asyncToGenerator(
   /*#__PURE__*/
   regeneratorRuntime.mark(function _callee3() {
@@ -241,8 +187,8 @@ function () {
   };
 }();
 
-var play = function play(filePath) {
-  player.play(filePath);
+var play = function play(fileName) {
+  player.play(fileName);
 };
 
 var pause = function pause() {
@@ -256,6 +202,22 @@ var resume = function resume() {
 var stop = function stop() {
   player.stop();
 };
+
+function playAction(args, callback) {
+  var filePath = args && args.options && args.options.file;
+
+  if (!filePath) {
+    /* eslint-disable prefer-destructuring */
+    var file = readFiles()[0];
+    /* eslint-enable prefer-destructuring */
+
+    filePath = file.path;
+  }
+
+  play(filePath);
+  this.log("Playing \u2013 ".concat(player.filePath()));
+  callback();
+}
 
 function pauseAction(args, callback) {
   pause();
@@ -305,34 +267,32 @@ function stopAction(args, callback) {
 });
 /** Add CLI Commands - End */
 
-app.get('/ls',
-/*#__PURE__*/
-function () {
-  var _ref7 = _asyncToGenerator(
+app.get('/ls', function () {
+  var _ref6 = _asyncToGenerator(
   /*#__PURE__*/
-  regeneratorRuntime.mark(function _callee6(req, res) {
+  regeneratorRuntime.mark(function _callee5(req, res) {
     var files;
-    return regeneratorRuntime.wrap(function _callee6$(_context6) {
+    return regeneratorRuntime.wrap(function _callee5$(_context5) {
       while (1) {
-        switch (_context6.prev = _context6.next) {
+        switch (_context5.prev = _context5.next) {
           case 0:
-            _context6.next = 2;
+            _context5.next = 2;
             return readFiles();
 
           case 2:
-            files = _context6.sent;
+            files = _context5.sent;
             res.send(files);
 
           case 4:
           case "end":
-            return _context6.stop();
+            return _context5.stop();
         }
       }
-    }, _callee6, this);
+    }, _callee5, this);
   }));
 
-  return function (_x6, _x7) {
-    return _ref7.apply(this, arguments);
+  return function (_x4, _x5) {
+    return _ref6.apply(this, arguments);
   };
 }());
 app.get('/', function (req, res) {
